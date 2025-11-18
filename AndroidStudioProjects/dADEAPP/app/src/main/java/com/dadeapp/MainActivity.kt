882d14a16dacc548b6e41810195f6f028bfc37af
Marcdave2 <dadelinelouis2@gmail.com>
2025-11-18 15:14:46 -0500

    lab 1terminer

---
+++ AndroidStudioProjects/dADEAPP/app/src/main/java/com/dadeapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.dadeapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
+        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+
+        flashcardQuestion.setOnClickListener {
+            flashcardQuestion.visibility = View.INVISIBLE
+            flashcardAnswer.visibility = View.VISIBLE
+        }
+
+        flashcardAnswer.setOnClickListener {
+            flashcardQuestion.visibility = View.VISIBLE
+            flashcardAnswer.visibility = View.INVISIBLE
+        }
+
+    }
+}
